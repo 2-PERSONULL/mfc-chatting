b67a299668ae4271cac854b806d899d8aec8ba2c
pockyjx <0127828b@gmail.com>
2024-06-13 19:49:35 +0900

    chore: swagger 설정

--- src/main/java/com/mfc/chatting/chat/presentation/ChatController.java
+++ src/main/java/com/mfc/chatting/chat/presentation/ChatController.java
@@ -16,7 +16,8 @@ import com.mfc.chatting.chat.domain.Message;
 import com.mfc.chatting.chat.dto.req.ChatReqDto;
 import com.mfc.chatting.chat.vo.req.ChatReqVo;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -25,19 +26,20 @@ import reactor.core.scheduler.Schedulers;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/chat")
-@CrossOrigin(origins = "*")
+@Tag(name = "chat", description = "채팅 서비스 컨트롤러")
 public class ChatController {
 	private final ChatService chatService;
 	private final ModelMapper modelMapper;
 
 	@GetMapping(value = "/room/{roomId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@Operation(summary = "채팅 조회 API", description = "채팅방 번호에 따른 채팅 목록 조회 (동작 안함 ㅎㅎ..)")
 	public Flux<Message> getChatByRoomId(
-		@PathVariable(value ="roomId") Long roomId
-	){
+		@PathVariable(value ="roomId") Long roomId){
 		return chatService.getChatByRoomId(roomId).subscribeOn(Schedulers.boundedElastic());
 	}
 
 	@PostMapping
+	@Operation(summary = "채팅 전송 API", description = "type = msg/card/image")
 	public Mono<Message> sendChat(@RequestBody ChatReqVo vo,
 			@RequestHeader(value = "UUID", defaultValue = "") String uuid){
 		return chatService.sendChat(modelMapper.map(vo, ChatReqDto.class),uuid);
---
+++ src/main/java/com/mfc/chatting/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.mfc.chatting.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+@OpenAPIDefinition(
+		info = @Info(
+				title = "My Fashion Coordinator",
+				description = "mfc 채팅 서비스 API 명세서",
+				version = "v1.0"
+		)
+)
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		SecurityScheme apiKey = new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.in(SecurityScheme.In.HEADER)
+				.name("Authorization")
+				.scheme("bearer")
+				.bearerFormat("JWT");
+
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer Token");
+
+		return new OpenAPI()
+				.addServersItem(new Server().url("/chatting-service"))
+				.components(new Components()
+						.addSecuritySchemes("Bearer Token", apiKey))
+				.security(Arrays.asList(securityRequirement));
+	}
+}
