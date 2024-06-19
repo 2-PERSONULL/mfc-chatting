82ead10751d44c0eb5602776ca3746dd331c1511
pockyjx <0127828b@gmail.com>
2024-06-19 12:09:47 +0900

    feat: 채팅 전송 시 상대방 unreadCount+1 구현

--- src/main/java/com/mfc/chatting/chat/application/ChatServiceImpl.java
+++ src/main/java/com/mfc/chatting/chat/application/ChatServiceImpl.java
@@ -1,15 +1,23 @@
 package com.mfc.chatting.chat.application;
 
+import static com.mfc.chatting.common.response.BaseResponseStatus.*;
+
 import java.time.Instant;
+import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mfc.chatting.chat.domain.Card;
+import com.mfc.chatting.chat.domain.ChatRoom;
+import com.mfc.chatting.chat.domain.Member;
 import com.mfc.chatting.chat.domain.Message;
 import com.mfc.chatting.chat.dto.req.ChatReqDto;
 import com.mfc.chatting.chat.infrastructure.ChatRepository;
+import com.mfc.chatting.chat.infrastructure.ChatRoomRepository;
+import com.mfc.chatting.common.exception.BaseException;
+import com.mfc.chatting.common.response.BaseResponseStatus;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +30,7 @@ import reactor.core.publisher.Mono;
 public class ChatServiceImpl implements ChatService{
 	private final ChatRepository chatRepository;
 	private final ObjectMapper objectMapper;
+	private final ChatRoomRepository chatRoomRepository;
 
 	@Override
 	public Flux<Message> getChat(String sender, String receiver) {
@@ -35,6 +44,9 @@ public class ChatServiceImpl implements ChatService{
 
 	@Override
 	public Mono<Message> sendChat(ChatReqDto dto, String uuid) {
+		ChatRoom chatRoom = chatRoomRepository.findById(dto.getRoomId())
+				.orElseThrow(() -> new BaseException(CHATROOM_NOT_FOUND));
+
 		String type = dto.getType();
 		Card card = null;
 		String msg = null;
@@ -44,6 +56,27 @@ public class ChatServiceImpl implements ChatService{
 		} else {
 			msg = (String) dto.getMsg();
 		}
+
+		List<Member> members = chatRoom.getMembers().stream()
+				.map(member -> {
+					if(!member.getMemberId().equals(uuid) && member.isEnterTimeBeforeExitTime()) {
+						member = Member.builder()
+								.memberId(member.getMemberId())
+								.unreadCount(member.getUnreadCount() + 1)
+								.exitTime(member.getExitTime())
+								.enterTime(member.getEnterTime())
+								.build();
+					}
+					return member;
+				})
+				.toList();
+
+		chatRoomRepository.save(ChatRoom.builder()
+				.id(chatRoom.getId())
+				.requestId(chatRoom.getRequestId())
+				.members(members)
+				.createdAt(chatRoom.getCreatedAt())
+				.build());
 
 		return chatRepository.save(Message.builder()
 			.type(type)
--- src/main/java/com/mfc/chatting/chat/infrastructure/ChatRoomRepository.java
+++ src/main/java/com/mfc/chatting/chat/infrastructure/ChatRoomRepository.java
@@ -1,5 +1,6 @@
 package com.mfc.chatting.chat.infrastructure;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
